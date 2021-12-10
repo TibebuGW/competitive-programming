b67e37ed518d329208eacb0b96a4d55a9d005678
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2021-12-10 16:03:59 +0300

    Create selection sort.js

---
+++ selection sort.js
@@ -0,0 +1,15 @@
+selectionSort(arr,n){
+   //code here
+   var j, min, temp
+   for(let i = 0; i < arr.length; i++){
+       j = i + 1
+       min = i
+       for(j; j < arr.length; j++){
+           if(arr[j] < arr[min]){
+               min = j
+           }
+       }
+       temp = arr[min]
+       arr[min] = arr[i]
+       arr[i] = temp
+   }
