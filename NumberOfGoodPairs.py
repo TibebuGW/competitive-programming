059536bb80f6cc2f8195369bc4075c4b70c3357d
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2021-12-16 13:35:59 +0300

    Create NumberOfGoodPairs.py

---
+++ NumberOfGoodPairs.py
@@ -0,0 +1,32 @@
+class Solution:
+        
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        def fact(y) -> int:
+            if(y == 1 or y == 0):
+                return 1
+            else:
+                return y*fact(y-1)
+            
+        toReturn = Counter(nums)
+        num = 0
+        
+        for x in list(toReturn.values()):
+            if x > 1:
+                num += fact(x)/(2*fact(x-2))
+            else: 
+                continue
+              
+        return int(num)
+      
+      
+############# Trial 1 ##########
+# class Solution:
+#   def numIdenticalPairs(self, nums: List[int]) -> int:
+#       toReturn = 0
+#       for i in range(0, len(nums)):
+#            for j in range(i+1, len(nums)):
+#                 if nums[i] == nums[j]:
+#                     toReturn += 1
+#       return toReturn
+    
+    
