7f2b96f67cd22d433d337c94c9b7cd5738d074a5
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2021-12-26 17:55:57 +0300

    Update LongestSubarray.py

--- LongestSubarray.py
+++ LongestSubarray.py
@@ -1,41 +1,67 @@
-class Solution:
-    def longestSubarray(self, nums: List[int], limit: int) -> int:
-        stack = [0]
-        def absDiff(arr: List[int])-> int:
-            return max(arr) - min(arr)
-        #nums.sort()
-              
 
 
-        
-        for i in range(0, len(nums)):
-            if len(nums)-i > stack[-1]:
-                for j in range(len(nums), i, -1):
-                    # print("*****")
-                    # print(nums[i:j])
-                    # print(absDiff(nums[i:j]))
+
+
+############# trial 2 #################
+
+# class Solution:
+#     def longestSubarray(self, nums: List[int], limit: int) -> int:
+#         result = 0
+#         arrMin = max(nums)
+#         if len(nums) == 1:
+#             return 1
+#         for i in range(0, len(nums)):
+#             arrMax = nums[i]
+#             arrMin = nums[i]
+#             if len(nums[i:len(nums)])+1 < result:
+#                 break
+#             for j in range(i+1, len(nums)):
+#                 # print("*****")
+#                 # print(nums[i:j+1])
+#                 # print(absDiff(nums[i:j]))
+                
+#                 if nums[j] >= arrMax:
+#                     arrMax = nums[j]
                     
-                    # if j-i < stack[-1]:
-                    #     break
+#                 if nums[j] <= arrMin:
+#                     arrMin = nums[j]
+                
+#                 if abs(arrMax-arrMin) > limit:
+#                     # print(j-i+1)
+#                     break
+#                 else:
+#                     # print("works: ", j-i+1)
+#                     result = max(result, j-i+1)
+
+#         return result
+
+############# trial 1 #################
+
+# class Solution:
+#     def longestSubarray(self, nums: List[int], limit: int) -> int:
+#         stack = [0]
+#         def absDiff(arr: List[int])-> int:
+#             return max(arr) - min(arr)
+#         for i in range(0, len(nums)):
+#             if len(nums)-i > stack[-1]:
+#                 for j in range(len(nums), i, -1):
+#                     # print("*****")
+#                     # print(nums[i:j])
+#                     # print(absDiff(nums[i:j]))
                    
-                    if absDiff(nums[i:j]) <= limit:
-                        length = j-i
-                        if stack and length <= stack[-1]:
-                            # print(stack)
-                            # stack.append(length)
-                            break
-                        else:
-                            while stack and stack[-1] < length:
-                                stack.pop()
-                            stack.append(j-i)
-                            # print(stack)
-            else:
-                break
-        #print(stack)
-        return stack[0]
+#                     if absDiff(nums[i:j]) <= limit:
+#                         length = j-i
+#                         if stack and length <= stack[-1]:
+#                             # print(stack)
+#                             break
+#                         else:
+#                             while stack and stack[-1] < length:
+#                                 stack.pop()
+#                             stack.append(j-i)
+#                             # print(stack)
+#             else:
+#                 break
+#         #print(stack)
+#         return stack[0]
         
-#         while nums and nums[-1] - nums[0] >= limit:
-#             nums.pop()
-#             print(nums)
-            
-#         return(len(nums))
+
