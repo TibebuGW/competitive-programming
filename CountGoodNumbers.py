39994716dc0c9fc0c0b989f0d81c52a45ecaba6c
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-01-10 01:01:26 +0300

    Update CountGoodNumbers.py

--- CountGoodNumbers.py
+++ CountGoodNumbers.py
@@ -1,11 +1,18 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         m = 1000000007
-        if n == 2:
-            return 20
-        if n == 1:
-            return 5
         
-        d = 5 if n%2 == 1 else 1
+        return (pow(5, ceil(n/2), m)*pow(4, n//2, m))%m
+  
+################ Trial 1:TLE ##################3
+# class Solution:
+#     def countGoodNumbers(self, n: int) -> int:
+#         m = 1000000007
+#         if n == 2:
+#             return 20
+#         if n == 1:
+#             return 5
         
-        return (20**(n//2)*d)%m
+#         d = 5 if n%2 == 1 else 1
+        
+#         return (20**(n//2)*d)%m
