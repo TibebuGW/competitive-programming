9c77bdc140bcb04f87784b4dd85cfe64c88069e3
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-01-16 23:06:49 +0300

    Create CodeFor1.py

---
+++ CodeFor1.py
@@ -0,0 +1,20 @@
+################ trial 1: MLE #############
+# a,b,c = map(int, input().split())
+# def dfs(x: int):
+#     if x == 0:
+#         return 0
+#     if x == 1:
+#         return 1
+    
+#     y = dfs(x//2)
+#     return str(y) + str(x%2) + str(y)
+    
+# def codeForOne(n:int, l:int, r:int)-> int:
+#     x = dfs(n)
+#     count = 0
+#     for i in range(l-1, r):
+#         if x[i] == '1':
+#             count += 1
+#     return count
+
+# print(codeForOne(a, b, c))
