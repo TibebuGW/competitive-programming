12acc4cb713a4054a6328ab0b4cb9e05252364cd
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-01-20 13:51:31 +0300

    Update MiddleNode.py

--- MiddleNode.py
+++ MiddleNode.py
@@ -1,3 +1,4 @@
+########################## Trial 1 #############################
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,10 +6,33 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        first = second = head
-        
-        while second and second.nexty:
-            first = first.next
-            second = second.next.next
+        temp1 = temp2 = head
+        count = 0
+        while temp1:
+            count += 1
+            temp1 = temp1.next
             
-        return first
+        for i in range(count//2):
+            temp2 = temp2.next
+            
+        return temp2
+
+
+
+########################## Better Solution ####################################
+
+
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         first = second = head
+        
+#         while second and second.nexty:
+#             first = first.next
+#             second = second.next.next
+            
+#         return first
