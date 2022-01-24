428033ffda5ce2f3c17f744bea4b0e8454733f21
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-01-24 18:40:03 +0300

    Update DeleteNodeInALinkedList.py

--- DeleteNodeInALinkedList.py
+++ DeleteNodeInALinkedList.py
@@ -20,3 +20,23 @@ class Solution:
                 node.next = None
             
 #         print(node)
+
+###################### best solution #########################
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        # print(node)
+        
+        node.val = node.next.val
+        node.next = node.next.next
+            
+        # print(node)
