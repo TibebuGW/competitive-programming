8c7c5c16060de9ca1b1f7fb5416bd0e17bd75c15
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-03-04 18:12:26 +0300

    Time: 236 ms (38.40%), Space: 16 MB (59.98%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -10,24 +10,38 @@ class Solution:
         in_bound = lambda row, col: 0 <= row < n and 0 <= col < m
         
         def dfs(x,y):
-            Os.append((x,y))
+            board[x][y] = "Y"
             for i in range(4):
                 tempx = x+directions[i][0]
                 tempy = y+directions[i][1]
-                if in_bound(tempx, tempy) and board[tempx][tempy] == "O" and (tempx,tempy) not in Os:
+                if in_bound(tempx, tempy) and board[tempx][tempy] == "O":
                     dfs(tempx, tempy)
             
         
         for i in range(n):
-            for j in range(m):
-                if board[i][j] == "O" and (i == 0 or j == 0 or i == n-1 or j == m-1):
-                    # print(board[i][j])
-                    dfs(i,j)
+            if board[i][0] == "O":
+                dfs(i,0)
+            if board[i][m-1] == "O":
+                dfs(i,m-1)
+                
+        for i in range(m):
+            if board[0][i] == "O":
+                dfs(0,i)
+            if board[n-1][i] == "O":
+                dfs(n-1,i)
+                
+            # for j in range(m):
+            #     if board[i][j] == "O" and (i == 0 or j == 0 or i == n-1 or j == m-1):
+            #         # print(board[i][j])
+            #         dfs(i,j)
                     
         for i in range(n):
             for j in range(m):
-                if board[i][j] == "O" and (i,j) not in Os:
+                if board[i][j] == "O":
                     board[i][j] = "X"
+                elif board[i][j] == "Y":
+                    board[i][j] = "O"
+                
                     
         # print(Os)
 
