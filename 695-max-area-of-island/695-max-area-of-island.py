2e15da17e2fb90adc0144a4ecc514f6d06be6bc7
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-03-04 16:40:32 +0300

    Time: 153 ms (78.97%), Space: 17.7 MB (26.01%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        result = [0]
+        n = len(grid)
+        m = len(grid[0])
+        in_bound = lambda row, col: 0 <= row < n and 0 <= col < m
+        directions = [[0,1], [0,-1], [-1,0], [1,0]]
+        
+        def dfs(x,y,count):
+            grid[x][y] = 2
+            count += 1
+
+            for i in range(len(directions)):
+                tempx = x+directions[i][0]
+                tempy = y+directions[i][1]
+                if in_bound(tempx,tempy) and grid[tempx][tempy] == 1:
+                    count += dfs(tempx, tempy, 0)
+                    
+            return count
+                    
+                    
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    result.append(dfs(i,j,0))
+                    # print(isConnected)
+                    
+        print(result)
+        return max(result)
