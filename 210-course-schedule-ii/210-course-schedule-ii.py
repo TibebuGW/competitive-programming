605340f785d872e370bb32db16fc1e2499302db7
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-04-08 18:43:39 +0300

    Time: 244 ms (9.42%), Space: 15.5 MB (66.93%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,31 @@
+from collections import deque
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        inDegree = [0]*numCourses
+        outDegree = defaultdict(set)
+        
+        for course, pre in prerequisites:
+            inDegree[course] += 1
+            outDegree[pre].add(course)
+            
+        # print(inDegree)
+        # print(outDegree)
+        result = []
+        queue = deque([])
+        for i in range(numCourses):
+            if inDegree[i] == 0:
+                queue.append(i)
+                result.append(i)
+                
+        count = 0
+        
+        while queue:
+            node = queue.popleft()
+            if node not in result: result.append(node)
+            count += 1
+            for j in outDegree[node]:
+                inDegree[j] -= 1
+                if inDegree[j] == 0:
+                    queue.append(j)
+                    
+        return result if count == numCourses else []
