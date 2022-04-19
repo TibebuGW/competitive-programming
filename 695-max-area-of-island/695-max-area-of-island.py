6f814ea4d5b87ba1a4a5567f4b2af01dae4dadab
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-04-19 18:41:33 +0300

    Time: 186 ms (51.70%), Space: 14.3 MB (84.26%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,29 +1,56 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        result = [0]
-        n = len(grid)
-        m = len(grid[0])
-        in_bound = lambda row, col: 0 <= row < n and 0 <= col < m
-        directions = [[0,1], [0,-1], [-1,0], [1,0]]
+        m = len(grid)
+        n = len(grid[0])
+        parent = [i for i in range(n*m)]
+        # print(parent)
+        directions = [[0,-1],[-1,0]]
+        in_range = lambda row, col: 0 <= row < m and 0 <= col < n
+        # graph = defaultdict(list)
         
-        def dfs(x,y,count):
-            grid[x][y] = 2
-            count += 1
-
-            for i in range(len(directions)):
-                tempx = x+directions[i][0]
-                tempy = y+directions[i][1]
-                if in_bound(tempx,tempy) and grid[tempx][tempy] == 1:
-                    count += dfs(tempx, tempy, 0)
+        # for i in range(n):
+        #     for j in range(m):
+                        
+                
+        rank = [grid[i][j] for i in range(m) for j in range(n)]
+        # print(rank)
+        
+        def find(a):
+            while a != parent[a]:
+                a = parent[a]
+            return a
+        
+        def union(a, b):
+            # print("num1:", a)
+            # print("num2:", b)
+            a = find(a)
+            b = find(b)
+            
+            if a != b:
+                if rank[a] > rank[b]:
+                    parent[b] = a
+                    rank[a] += rank[b]
+                else:
+                    parent[a] = b
+                    rank[b] += rank[a]
+            
+            # print(rank)
                     
-            return count
-                    
-                    
-        for i in range(n):
-            for j in range(m):
+        for i in range(m):
+            for j in range(n):
+                # print("subject:", (i*m)+j)
                 if grid[i][j] == 1:
-                    result.append(dfs(i,j,0))
-                    # print(isConnected)
+                    # print("i:", i, "j:", j, "grid value:", grid[i][j])
+                    for k in range(2):
+                        nei_i = i+directions[k][0]
+                        nei_j = j+directions[k][1]
+                        if in_range(nei_i,nei_j) and grid[nei_i][nei_j] == 1:
+                            union((i*n)+j, (nei_i*n)+nei_j)
                     
-        # print(result)
-        return max(result)
+        # print(parent)
+        # print(rank)
+        # mmax = 0
+        # print(grid)
+        return max([rank[find(i)] for i in parent])
+        # d = collections.Counter(parent)
+        # print(d)
