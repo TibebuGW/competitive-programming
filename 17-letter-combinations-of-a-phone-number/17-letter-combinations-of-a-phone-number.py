ed0dbe3c2d364b313c4c84e9afe8d5548024ab79
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-05-15 16:36:56 +0300

    Time: 35 ms (75.39%), Space: 14.1 MB (30.33%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -4,14 +4,24 @@ class Solution:
         if digits == "":
             return []
         d = {"2":["a", "b", "c"], "3":["d", "e", "f"], "4":["g", "h", "i"], "5":["j", "k", "l"], "6":["m", "n", "o"], "7":["p", "q", "r", "s"], "8":["t", "u", "v"], "9":["w", "x", "y", "z"]}
-        arr = []
-        result = []
-        for digit in digits:
-            arr.append(d[digit])
         
-        for i in itertools.product(*arr):
-            result.append("".join(i))
+        result = []
+        i_str = ""
+        
+        def dfs(i, path):
+            if i == len(digits):
+                result.append(path)
+                return
+            else:
+                for char in d[digits[i]]:
+                    path += char
+                    dfs(i+1, path)
+                    path = path[:-1]
+        
+        
+        
+        for char in d[digits[0]]:
+            i_str = char
+            dfs(1, i_str)
             
         return result
-        
-        
