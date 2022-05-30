1e970f67a3e7f778b2dc7f3c8f085a7b1ac7c225
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-05-30 18:53:25 +0300

    Time: 47 ms (44.40%), Space: 13.9 MB (53.34%) - LeetHub

---
+++ 258-add-digits/258-add-digits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        
+        while len(str(num)) != 1:
+            num = sum([int(x) for x in str(num)])
+        
+        return num
