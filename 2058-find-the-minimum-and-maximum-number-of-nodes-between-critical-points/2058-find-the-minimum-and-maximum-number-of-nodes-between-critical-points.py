0432bb8bde088da66fcb92474c293592122b1464
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-08-11 20:28:51 +0300

    Time: 1060 ms (88.89%), Space: 54.2 MB (86.02%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -10,9 +10,12 @@ class Solution:
         node = head.next
         prev = head
         indices = []
+        min_ = float('inf')
         while node.next:
             if node.val > prev.val and node.val > node.next.val or node.val < prev.val and node.val < node.next.val:
                 indices.append(counter)
+                if len(indices) >= 2:
+                    min_ = min(min_, indices[-1]-indices[-2])
             node = node.next
             prev = prev.next
             counter += 1
@@ -20,9 +23,4 @@ class Solution:
         if len(indices) <= 1:
             return [-1,-1]
         max_ = indices[-1]-indices[0]
-        min_ = float('inf')
-        for i in range(1, len(indices)):
-            if indices[i]-indices[i-1] < min_:
-                min_ = indices[i]-indices[i-1]
-        # self.printer(original)
         return [min_, max_]
