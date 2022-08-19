26f3d42e538e58bb5898e33cbd4376094021447d
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-08-19 11:58:05 +0300

    Time: 1332 ms (14.02%), Space: 27.9 MB (91.47%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def counter(self, nums, lowerlimit, upperlimit):
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] > lowerlimit and nums[i] <= upperlimit:
+                count += 1
+
+        return count
+                
+    def findDuplicate(self, nums: List[int]) -> int:
+        left = 1
+        right = max(nums)
+        lowerlimit = 0
+        best = 0
+        
+        while left <= right:
+            mid = (left+right)//2
+            # print(self.counter(nums, lowerlimit, mid))
+            
+            if self.counter(nums, lowerlimit, mid) > mid-lowerlimit:
+                best = mid
+                right = mid-1
+            else:
+                lowerlimit = mid
+                left = mid+1
+            
+        return best
