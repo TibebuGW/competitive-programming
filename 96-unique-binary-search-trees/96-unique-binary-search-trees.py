cbfc3a7d3ce6166bbc19131f4d0391b3580dd7c9
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-08-19 18:10:22 +0300

    Time: 54 ms (31.37%), Space: 13.8 MB (97.05%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,26 +1,5 @@
 class Solution:
-    def numTrees(self, m: int) -> int:
-        dp = {0:1, 1:1, 2:2}
-        def dfs(n):
-            if n in dp:
-                return dp[n]
-            total = 0
-            for i in range(1, n+1):
-                a = b = 0
-                if i-1 in dp:
-                    a = dp[i-1]
-                else:
-                    a = self.numTrees(i-1)
-                    dp[i-1] = a
-                
-                if n-i in dp:
-                    b = dp[n-i]
-                else:
-                    b = self.numTrees(n-i)
-                    dp[n-i] = b
-                    
-                total += a*b
+    def numTrees(self, n: int) -> int:
         
-            return total
         
-        return dfs(m)
+        return int(math.factorial(2*n)/(math.factorial(n)*math.factorial(n+1)))
