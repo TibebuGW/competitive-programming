2bb21d7a7d7c4b438d336b4ad8f8bcf5d48d9134
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-08-25 23:40:57 +0300

    Time: 64 ms (76.31%), Space: 16.4 MB (17.54%) - LeetHub

---
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        arr = []
+        
+        def dfs(node):
+            nonlocal arr
+            if not node:
+                return
+            
+            for child in node.children:
+                dfs(child)
+            
+            arr.append(node.val)
+            
+        dfs(root)
+        return arr
