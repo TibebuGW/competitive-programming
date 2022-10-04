015f7dbb2d7e00cce83eb6d3e589062c3848ad35
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-10-04 14:54:01 +0300

    Time: 227 ms (90.70%), Space: 16.1 MB (15.76%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.py
+++ 503-next-greater-element-ii/503-next-greater-element-ii.py
@@ -4,21 +4,11 @@ class Solution:
         stack = []
         d = defaultdict(lambda: -1)
         
-        i = 0
-        while i < len(nums):
-            
-            while stack and nums[i] > nums[stack[-1]]:
-                d[stack.pop()] = nums[i]
-                
-            if stack and i in stack:
-                break
-                
-            stack.append(i)
-            
-            if i == len(nums)-1:
-                i = 0
-            else:
-                i += 1
+        for j in range(2):
+            for i in range(len(nums)):
+                while stack and nums[i] > nums[stack[-1]]:
+                    d[stack.pop()] = nums[i]
+                stack.append(i)
         
         for i in range(len(nums)):
             ans.append(d[i])
