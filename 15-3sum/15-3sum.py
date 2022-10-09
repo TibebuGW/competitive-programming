614f735e164ded8c59377ca72087e150ab6bc2e2
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-10-09 15:16:42 +0300

    Time: 2122 ms (41.68%), Space: 18.2 MB (21.41%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -20,9 +20,9 @@ class Solution:
                     ans.append([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
-                    while l < r and nums[l] == nums[l-1]:
+                    while l < len(nums) and nums[l] == nums[l-1]:
                         l += 1
-                    while r > l and nums[r] == nums[r+1]:
+                    while r > 0 and nums[r] == nums[r+1]:
                         r -= 1
         
         return ans
