d2b8efe39443aa46629ec4c09b5637d14eb3ef03
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-10-10 10:00:56 +0300

    Time: 61 ms (23.95%), Space: 13.8 MB (61.62%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,12 +1,22 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
-        if len(palindrome) == 1:
-            return ""
+        last = -1
         arr = list(palindrome)
-        for i in range(len(palindrome)//2):
-            if arr[i] != "a":
+
+        for i, char in enumerate(arr):
+            if char == "a":
+                arr[i] = "b"
+                if arr != arr[::-1]:
+                    last = i
                 arr[i] = "a"
-                return "".join(arr)
-        
-        arr[-1] = "b"
-        return "".join(arr)
+            else:
+                arr[i] = "a"
+                if arr == arr[::-1]:
+                    arr[i] = char
+                else:
+                    return "".join(arr)
+        if last != -1:
+            arr[last] = "b"
+            return "".join(arr)
+        else:
+            return ""
