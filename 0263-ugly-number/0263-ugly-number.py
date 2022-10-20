6d4b570068a23c72b14a7dea1831227f2a4904d0
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-10-20 14:48:23 +0300

    Time: 37 ms (87.67%), Space: 14 MB (12.92%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n == 0:
+            return False
+        if n == 1 or n == 2 or n == 3 or n == 5:
+            return True
+        else:
+            for val in [2,3,5]:
+                if n%val == 0:
+                    if self.isUgly(n//val):
+                        return True
+            
+            return False
