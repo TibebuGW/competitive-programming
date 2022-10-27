82b817e92d6ae8db2dfc25580f295e435557b27d
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-10-27 10:20:23 +0300

    Time: 3048 ms (5.01%), Space: 35.5 MB (5.33%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -5,9 +5,8 @@ class Solution:
             prefix.append(num+prefix[-1])
         
         d = {0:0}
-        if prefix[1]%k not in d:
-            d[prefix[1]%k] = 1
-        for i in range(2,len(prefix)):
+        
+        for i in range(1,len(prefix)):
             if prefix[i]%k not in d:
                 d[prefix[i]%k] = i
             else:
