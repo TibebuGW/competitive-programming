a6be027ecb534dc87057fc95deca6fb57c237147
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-10-27 16:15:53 +0300

    Time: 957 ms (7.07%), Space: 27.3 MB (57.66%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -3,13 +3,12 @@ class Solution:
         
         for i in range(len(nums)):
             while 1<=nums[i]<=len(nums) and nums[i] != nums[nums[i]-1]:
-                temp1 = nums[i]
-                temp2 = nums[nums[i]-1]
-                nums[nums[i]-1] = temp1
-                nums[i] = temp2
+                # print(nums, i, nums[i], nums[nums[i]])
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
                 
-        for i in range(len(nums)):
-            if nums[i] != i+1:
-                return i+1
         
+        for i in range(len(nums)):
+            if i+1 != nums[i]:
+                return i+1
+    
         return len(nums)+1
