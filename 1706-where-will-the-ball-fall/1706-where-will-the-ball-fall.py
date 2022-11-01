14f6ef8c04532d9dbf1a9d79228265e964b73d00
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-11-01 11:29:11 +0300

    Time: 577 ms (17.66%), Space: 14.9 MB (6.83%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -9,22 +9,32 @@ class Solution:
         if col < n-1 and grid[row][col] == 1 and grid[row][col+1] == -1:
             return True
         return False
+   
     
     def findBall(self, grid: List[List[int]]) -> List[int]:
         m = len(grid)
         n = len(grid[0])
         ans = [-1]*n
-        for i in range(n):
-            if self.stuck(grid, 0, i):
-                continue
-            else:
-                next_col = i+1 if grid[0][i] == 1 else i-1
-            for j in range(1, m):
-                if self.stuck(grid, j, next_col):
-                    next_col = -1
-                    break
-                else:
-                    next_col = next_col+1 if grid[j][next_col] == 1 else next_col-1
-            ans[i] = next_col
+        memo = [[0 for _ in range(n+1)] for _ in range(m+1)]
         
+        def dfs(row, col):
+            if memo[row][col] != 0:
+                return memo[row][col]
+            if row == len(grid):
+                return col
+
+            # print(row, col)
+            if self.stuck(grid, row, col):
+                return -1
+            else:
+                next_col = col+1 if grid[row][col]==1 else col-1
+                memo[row][col] = dfs(row+1, next_col)
+                return memo[row][col]
+        
+        for i in range(n):
+            # print(i)
+            ans[i] = dfs(0, i)
+            
         return ans
+        
+        
