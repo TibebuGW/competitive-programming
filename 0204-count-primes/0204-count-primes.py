2634ac30f145a5215fa57dfc466d4cecdcb8fd3c
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-11-04 11:58:37 +0300

    Time: 8698 ms (11.90%), Space: 52.7 MB (84.69%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -3,14 +3,14 @@ class Solution:
         if n == 0 or n == 1:
             return 0
         primes = [True]*(n)
-        limit = math.floor(math.sqrt(n))
         primes[0] = False
         primes[1] = False
-        for i in range(2, limit+1):
+        i = 2
+        while i*i < n:
             if primes[i]:
                 for j in range(i*i, n, i):
                     primes[j] = False
-                
+            i += 1
         count = 0
         for val in primes:
             if val == True:
