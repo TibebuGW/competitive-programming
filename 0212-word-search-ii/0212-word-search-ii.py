088d5e5a677dd62e6bc58c2e347e0322c35a443a
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-11-06 22:34:24 +0300

    Time: 3708 ms (37.90%), Space: 15.6 MB (63.74%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,56 +1,52 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        WORD_KEY = '$'
-        
-        trie = {}
+        # insert all words into the prefix tree 
+        root = {}
         for word in words:
-            node = trie
-            for letter in word:
-                # retrieve the next node; If not found, create a empty node.
-                node = node.setdefault(letter, {})
-            # mark the existence of a word in trie node
-            node[WORD_KEY] = word
+            cur = root
+            for char in word:
+                if char not in cur:
+                    cur[char] = {}
+                cur = cur[char]
+            cur["WORD"] = word
         
-        rowNum = len(board)
-        colNum = len(board[0])
+        # declare necessary stuff
+        ans = []
+        n = len(board)
+        m = len(board[0])
+        in_bound = lambda row, col: 0<=row<n and 0<=col<m
+        directions = [[-1,0],[1,0],[0,1],[0,-1]]
+        visited = set()
         
-        matchedWords = []
-        
-        def backtracking(row, col, parent):    
-            
+        # starting from a letter on the board, see if you can form a word from the word list
+        def dfs(row, col, node):
             letter = board[row][col]
-            currNode = parent[letter]
-            
-            # check if we find a match of word
-            word_match = currNode.pop(WORD_KEY, False)
+            curNode = node[letter]
+            word_match = curNode.pop("WORD", False)
             if word_match:
-                # also we removed the matched word to avoid duplicates,
-                #   as well as avoiding using set() for results.
-                matchedWords.append(word_match)
+                ans.append(word_match)
             
-            # Before the EXPLORATION, mark the cell as visited 
-            board[row][col] = '#'
-            
-            # Explore the neighbors in 4 directions, i.e. up, right, down, left
-            for (rowOffset, colOffset) in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-                newRow, newCol = row + rowOffset, col + colOffset     
-                if newRow < 0 or newRow >= rowNum or newCol < 0 or newCol >= colNum:
+            board[row][col] = "#"
+            for row_move, col_move in [[-1,0],[1,0],[0,1],[0,-1]]:
+                new_row = row+row_move
+                new_col = col+col_move
+                
+                if new_row < 0 or new_row >= n or new_col < 0 or new_col >= m:
                     continue
-                if not board[newRow][newCol] in currNode:
+                if not board[new_row][new_col] in curNode:
                     continue
-                backtracking(newRow, newCol, currNode)
-        
-            # End of EXPLORATION, we restore the cell
+                    
+                cur_char = board[new_row][new_col]
+                dfs(new_row, new_col, curNode)
+            if not curNode:
+                node.pop(letter)
+                    
             board[row][col] = letter
+            return
         
-            # Optimization: incrementally remove the matched leaf node in Trie.
-            if not currNode:
-                parent.pop(letter)
-
-        for row in range(rowNum):
-            for col in range(colNum):
-                # starting from each of the cells
-                if board[row][col] in trie:
-                    backtracking(row, col, trie)
-        
-        return matchedWords    
+        # if a letter on the board is a start of a word, do dfs on it
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] in root:
+                    dfs(i, j, root)
+        return ans
