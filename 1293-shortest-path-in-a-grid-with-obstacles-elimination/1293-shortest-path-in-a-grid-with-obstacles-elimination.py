c3c2af0e183df2e78e5b5379c093737f917588bb
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-11-08 11:10:09 +0300

    Time: 831 ms (39.05%), Space: 22.5 MB (10.91%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -3,12 +3,12 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         min_ = float('inf')
-        queue = deque([(0, 0, 0)])
+        queue = deque([(0, 0, k)])
         last = (n-1, m-1)
         level = 0
         in_range = lambda row, col: 0<=row<n and 0<=col<m
         directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-        visited = set((0, 0, 0))
+        visited = set((0, 0, k))
         
         while queue:
             size = len(queue)
@@ -21,8 +21,8 @@ class Solution:
                         new_row = node[0] + row_move
                         new_col = node[1] + col_move
                         if in_range(new_row, new_col):
-                            obstacle_so_far = node[2]+grid[new_row][new_col]
-                            if (new_row, new_col, obstacle_so_far) not in visited and obstacle_so_far <= k:
+                            obstacle_so_far = node[2]-grid[new_row][new_col]
+                            if (new_row, new_col, obstacle_so_far) not in visited and obstacle_so_far >= 0:
                                 visited.add((new_row, new_col, obstacle_so_far))
                                 queue.append((new_row, new_col, obstacle_so_far))
             level += 1
