66704d772f3f2d6795cbd27a132c046a9d670683
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-11-08 16:44:41 +0300

    Time: 74 ms (17.77%), Space: 13.9 MB (72.02%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.py
+++ 0401-binary-watch/0401-binary-watch.py
@@ -1,52 +1,18 @@
 class Solution:
-    def __init__(self):
-        self.minute_array = []
-        self.hour_array = []
-        
-    def hour_combination(self, arr, k, total, index):
-        if k >= 4:
-            return
-        
-        if k == 0:
-            if total<12:
-                self.hour_array.append(str(total))
-            return
-        
-        for i in range(index, len(arr)):
-            total += arr[i]
-            self.hour_combination(arr, k-1, total, i+1)
-            total -= arr[i]
-            
-    def minute_combination(self, arr, k, total, index):
-        if k >= 6:
-            return
-        
-        if k == 0:
-            if 10<=total<=59:
-                self.minute_array.append(str(total))
-            elif 0<=total<10:
-                self.minute_array.append("0"+str(total))
-            return
-                
-        for i in range(index, len(arr)):
-            total += arr[i]
-            self.minute_combination(arr, k-1, total, i+1)
-            total -= arr[i]
-            
-    def readBinaryWatch(self, turnedOn: int) -> List[str]:        
+    def bitCounter(self, num):
+        count = 0
+        while num:
+            count += num&1
+            num >>= 1
+        return count
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
         ans = []
-        minute_k = turnedOn
-        hour_k = 0
-        for i in range(turnedOn+1):
-            self.minute_combination([1, 2, 4, 8, 16, 32], minute_k, 0, 0)
-            self.hour_combination([1, 2, 4, 8], hour_k, 0, 0)
-            
-            for hour in self.hour_array:
-                for minute in self.minute_array:
-                    ans.append(hour+":"+minute)
-            self.minute_array = []
-            self.hour_array = []
-            minute_k -= 1
-            hour_k += 1
-        ans.sort()
+        for hour in range(12):
+            for minute in range(60):
+                if self.bitCounter(hour)+self.bitCounter(minute) == turnedOn:
+                    if minute<10:
+                        minute = f'0{minute}'
+                    ans.append(f'{hour}:{minute}')
+                    
+                    
         return ans
