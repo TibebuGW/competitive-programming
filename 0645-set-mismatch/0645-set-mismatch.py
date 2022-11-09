56c2ed49e1852faf20caba6ac5c3311521dc0547
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-11-09 18:31:25 +0300

    Time: 617 ms (11.64%), Space: 15.2 MB (98.54%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,14 +1,35 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        ans = []
-        total = 0
-        for i, num in enumerate(nums):
-            if nums[abs(num)-1] < 0:
-                ans.append(abs(num))
+        bitmask = 0
+        for num in nums:
+            bitmask ^= num
+            
+        for num in range(1, len(nums)+1):
+            bitmask ^= num
+        
+        checker = 1
+        while True:
+            if bitmask&checker:
+                break
+            checker <<= 1
+        
+        division_1 = division_2 = 0
+        another_division_1 = another_division_2 = 0
+        for num in range(1, len(nums)+1):
+            if checker & num:
+                division_1 ^= num
             else:
-                nums[abs(num)-1] *= -1
-            total += abs(num)    
-        n = len(nums)
-        real_total = (n*(n+1))//2
-        ans.append(real_total-(total-ans[0]))
-        return ans
+                division_2 ^= num
+        
+        for num in nums:
+            if checker & num:
+                another_division_1 ^= num
+            else:
+                another_division_2 ^= num
+        
+        num1 = division_1^another_division_1
+        num2 = division_2^another_division_2
+        for num in nums:
+            if num == num2:
+                return [num2, num1]
+        return [num1, num2]
