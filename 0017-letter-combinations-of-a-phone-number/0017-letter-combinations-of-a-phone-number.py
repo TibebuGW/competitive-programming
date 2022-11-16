05630b6dbc88030047b18b8daa40124b9f5453d8
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-11-16 17:09:28 +0300

    Time: 36 ms (87.20%), Space: 14 MB (32.07%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,23 @@
+import itertools
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        d = {"2":["a", "b", "c"], "3":["d", "e", "f"], "4":["g", "h", "i"], "5":["j", "k", "l"], "6":["m", "n", "o"], "7":["p", "q", "r", "s"], "8":["t", "u", "v"], "9":["w", "x", "y", "z"]}
+        
+        ans = []
+        
+        def backtrack(path = [], index = 0):
+            if len(path) == len(digits):
+                ans.append("".join(path))
+                return
+            
+            for char in d[digits[index]]:
+                path.append(char)
+                backtrack(path, index+1)
+                path.pop()
+        
+        backtrack()
+        return ans
+            
+        
