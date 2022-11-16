cc959e1771499fe73f104d1041ba84181d0a0acb
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-11-16 12:13:16 +0300

    Time: 105 ms (80.71%), Space: 21.5 MB (46.79%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -6,10 +6,45 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
         
-        def dfs(node):
+        def depthCounter(node):
             if not node:
                 return 0
-            return 1 + dfs(node.left) + dfs(node.right)
+            return 1 + depthCounter(node.left)
+        
+        depth = depthCounter(root)
+        
+        
+        
+        
+        def dfs(node, left_end, right_end, index):
+            if not node:
+                return False
             
-        return dfs(root)
+            if left_end == right_end:
+                return True
+            
+            mid = (left_end+right_end)//2
+            if index <= mid:
+                return dfs(node.left, left_end, mid, index)
+            else:
+                return dfs(node.right, mid+1, right_end, index)
+            
+            
+        best = -1
+        l = 0
+        r = (2**(depth-1))-1
+        left = 0
+        right = (2**(depth-1))-1
+        count = right
+        while left <= right:
+            mid = (left+right)//2
+            mid_value = dfs(root, l, r, mid)
+            if mid_value:
+                left = mid+1
+                best = mid
+            else:
+                right = mid-1
+        return count + best + 1
