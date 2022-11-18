f6bfc85c93a8add57037453519f7401a4298b2e5
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-11-18 12:03:49 +0300

    Time: 1590 ms (70.57%), Space: 42.3 MB (50.05%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -8,17 +8,13 @@ class Solution:
                     if j-i <= 1 or dp[i+1][j-1]:
                         dp[i][j] = 1
         
-        @lru_cache(None)
-        def solve(start = 0):
-            if start == len(s):
-                return 0
-            
-            ans = float('inf')
-            
-            for end in range(start, len(s)):
-                if dp[start][end]:
-                    ans = min(ans, solve(end+1))
-            
-            return ans+1
-    
-        return solve() - 1
+        main_dp = [-1]*(n+1)
+        
+        for i in range(n-1, -1, -1):
+            min_ = float(inf)
+            for j in range(i, n):
+                if dp[i][j]:
+                    min_ = min(min_, main_dp[j+1]+1)
+            main_dp[i] = min_
+        
+        return main_dp[i]
