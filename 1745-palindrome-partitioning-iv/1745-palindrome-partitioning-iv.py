1902794104c9ff8173d07c4e8e60fdde80c09452
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-11-18 22:14:28 +0300

    Time: 3213 ms (81.02%), Space: 45.4 MB (59.85%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.py
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.py
@@ -9,18 +9,9 @@ class Solution:
                     if j-i <= 1 or is_palindrome[i+1][j-1]:
                         is_palindrome[i][j] = 1
         
-        @lru_cache(None)
-        def solve(start = 0, partitions = 0):
-            if n-start < 3-partitions or partitions > 3:
-                return False
-            
-            if start == n and partitions == 3:
-                return True
-            
-            for end in range(start, n):
-                if is_palindrome[start][end] and solve(end + 1, partitions + 1):
+        for i in range(1, n):
+            for j in range(i, n-1):
+                if is_palindrome[0][i-1] and is_palindrome[i][j] and is_palindrome[j+1][n-1]:
                     return True
-            
-            return False
         
-        return solve()
+        return False
