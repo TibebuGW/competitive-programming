3f9314e8726b92fc37f239bc8deae4512c10a445
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-11-30 14:16:56 +0300

    Time: 623 ms (94.59%), Space: 70.9 MB (6.55%) - LeetHub

---
+++ 0741-cherry-pickup/0741-cherry-pickup.py
@@ -0,0 +1,31 @@
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        in_range = lambda row, col: 0 <= row < n and 0 <= col < n
+        
+        @lru_cache(None)
+        def dp(r1, c1, r2, c2):
+            if not in_range(r1, c1) or not in_range(r2, c2) or grid[r1][c1] == -1 or grid[r2][c2] == -1:
+                return float('-inf')
+            
+            if r1 == n-1 and c1 == n-1 or r2 == n-1 and c2 == n-1:
+                return grid[-1][-1]
+            
+            cur_cherries = 0
+            if r1 == r2 and c1 == c2:
+                cur_cherries = grid[r1][c1]
+            else:
+                cur_cherries = grid[r1][c1] + grid[r2][c2]
+            
+            next_cherries = float('-inf')
+            
+            case1 = dp(r1 + 1, c1, r2 + 1, c2)
+            case2 = dp(r1, c1 + 1, r2, c2 + 1)
+            case3 = dp(r1 + 1, c1, r2, c2 + 1)
+            case4 = dp(r1, c1 + 1, r2 + 1, c2)
+            
+            next_cherries = max(case1, case2, case3, case4)
+            
+            return cur_cherries + next_cherries
+    
+        return max(0, dp(0, 0, 0, 0))
