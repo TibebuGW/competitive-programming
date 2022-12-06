c3dd1e2387127e980bbb9ceb643199dc823904f1
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-12-06 15:43:19 +0300

    Time: 854 ms (97.33%), Space: 60.2 MB (68.98%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -9,11 +9,21 @@ class Solution:
             if index == n or limit == 0:
                 return 0
             
+            l = index
+            r = n - 1
+            best = n
+            while l <= r:
+                mid = (l+r)//2
+                if augumented_events[mid][0] >= day:
+                    r = mid - 1
+                    best = mid
+                else:
+                    l = mid + 1
             max_ = 0
-            for i in range(index, n):
+            
+            for i in range(best, n):
                 cur_start, cur_end, cur_value = augumented_events[i]
-                if cur_start >= day:
-                    max_ = max(max_, dp(i + 1, limit - 1, cur_end + 1) + cur_value)
+                max_ = max(max_, dp(i + 1, limit - 1, cur_end + 1) + cur_value)
             
             return max_
         
