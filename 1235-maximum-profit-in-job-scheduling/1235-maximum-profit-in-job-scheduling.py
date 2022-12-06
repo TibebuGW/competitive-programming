aebcb752019c6fc4d56c4fec4be124ff254c7d4a
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-12-06 15:24:06 +0300

    Time: 1538 ms (37.10%), Space: 25.7 MB (88.06%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        events = list(zip(startTime, endTime, profit)) 
+        events.sort()
+        events.append((50001,50002,0))
+        n = len(events)
+        dp = [0]*(n)
+        
+        for i in range(n-2, -1 ,-1):
+            cur_start, cur_end, cur_profit = events[i]
+            pick = cur_profit
+            for j in range(i + 1, n):
+                start, end, profit = events[j]
+                if start >= cur_end:
+                    pick += dp[j]
+                    break
+            
+            dont_pick = dp[i+1]
+            dp[i] = max(pick, dont_pick)
+        
+        return dp[0]
