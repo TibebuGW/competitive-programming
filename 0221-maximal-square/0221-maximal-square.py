5e6f1aaca1f232687386308d034904828f0e21ec
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-12-11 17:30:44 +0300

    Time: 1863 ms (42.09%), Space: 16.8 MB (96.32%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.py
@@ -0,0 +1,43 @@
+class Solution:
+    def maxArea(self, arr):
+        left = [-1]*(len(arr))
+        stack = []
+        for i in range(len(arr)-1,-1, -1):
+            while stack and arr[stack[-1]] > arr[i]:
+                index = stack.pop()
+                left[index] = i
+            stack.append(i)
+            
+        right = [len(arr)]*(len(arr))
+        stack = []
+        for i in range(len(arr)):
+            while stack and arr[stack[-1]] > arr[i]:
+                index = stack.pop()
+                right[index] = i
+            stack.append(i)
+        
+        max_ans = 0
+        
+        for i in range(len(arr)):
+            width = right[i] - left[i] - 1
+            height = arr[i]
+            if width >= height:
+                max_ans = max(max_ans, height * height)
+        
+        return max_ans
+    
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        ans = 0
+        n = len(matrix)
+        m = len(matrix[0])
+        arr = [0]*m
+        
+        for row in matrix:
+            for i in range(m):
+                if int(row[i]):
+                    arr[i] += 1
+                else:
+                    arr[i] = 0
+            ans = max(ans, self.maxArea(arr))
+        
+        return ans
