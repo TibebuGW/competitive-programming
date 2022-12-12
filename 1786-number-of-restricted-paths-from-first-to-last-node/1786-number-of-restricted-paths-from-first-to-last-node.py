019f617c8b8f4bab17c4f4b5c3a054390b9e8f40
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-12-12 22:12:46 +0300

    Time: 6137 ms (7.77%), Space: 79.8 MB (6.40%) - LeetHub

---
+++ 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countRestrictedPaths(self, n: int, edges: List[List[int]]) -> int:
+        MOD = 10**9 + 7
+        graph = defaultdict(list)
+        for src, dst, weight in edges:
+            graph[src - 1].append((dst - 1, weight))
+            graph[dst - 1].append((src - 1, weight))
+        
+        distances = [float('inf')]*n
+        distances[-1] = 0
+        
+        queue = [(0, n - 1)] # shortest path to node n
+        
+        while queue:
+            distance_so_far, node = heappop(queue)
+            
+            for nei, weight in graph[node]:
+                if distance_so_far + weight < distances[nei]:
+                    distances[nei] = distance_so_far + weight
+                    heappush(queue, (distances[nei], nei))
+        
+        @lru_cache(None)
+        def dp(node = 0):
+            if node == n - 1:
+                return 1
+            
+            ans = 0
+            for nei, weight in graph[node]:
+                if distances[node] > distances[nei]:
+                    ans += dp(nei)
+            
+            return ans
+        
+        return dp() % MOD
