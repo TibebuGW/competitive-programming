c8fb8f01eae72634ddef91b93d8220f02e49e31e
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-12-19 22:32:09 +0300

    Time: 965 ms (58.63%), Space: 18.5 MB (31.58%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,33 +1,25 @@
-from collections import deque
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         m = len(matrix)
         n = len(matrix[0])
-        in_degree = {(i, j): 0 for i in range(m) for j in range(n)}
-        graph = defaultdict(list)
         directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
         in_range = lambda row, col: 0 <= row < m and 0 <= col < n
         
+        @lru_cache(None)
+        def dp(i, j):
+            
+            max_ = 0
+            
+            for dr, dy in directions:
+                r, c = dr + i, dy + j
+                if in_range(r, c) and matrix[r][c] > matrix[i][j]:
+                    max_ = max(max_, dp(r, c))
+            
+            return max_ + 1
+        
+        maximum = 1
         for i in range(m):
             for j in range(n):
-                for row_move, col_move in directions:
-                    row = i + row_move
-                    col = j + col_move
-                    if in_range(row, col) and matrix[i][j] < matrix[row][col]:
-                        in_degree[(row, col)] += 1
-                        graph[(i,j)].append((row, col))
+                maximum = max(maximum, dp(i, j))
         
-        @lru_cache(None)
-        def dfs(i, j):
-            max_ = 1
-            for nei in graph[(i, j)]:
-                max_ = max(max_, dfs(nei[0], nei[1]) + 1)
-            return max_
-        
-        ans = 0
-        for node, in_degree_value in in_degree.items():
-            if in_degree_value == 0:
-                # print(bfs(node[0], node[1]), node)
-                ans = max(ans, dfs(node[0], node[1]))
-        
-        return ans
+        return maximum
