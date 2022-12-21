71df7a53cab1760ede8971e01b79409c5fe9dcf6
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-12-21 10:47:09 +0300

    Time: 3733 ms (31.22%), Space: 17.6 MB (61.52%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,15 +1,16 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        graph = defaultdict(set)
-        for pre, curr in prerequisites:
-            graph[curr].add(pre)
-            graph[curr].update(graph[pre])
+        is_prerequisite = [[False for _ in range(numCourses)] for i in range(numCourses)]
         
-        for pre, curr in prerequisites[::-1]:
-            graph[curr].add(pre)
-            graph[curr].update(graph[pre])
-        ans = []
-        for u, v in queries:
-            ans.append(u in graph[v])
+        for i in range(numCourses):
+            is_prerequisite[i][i] = True
+            
+        for pre, course in prerequisites:
+            is_prerequisite[pre][course] = True
         
-        return ans
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    is_prerequisite[i][j] = is_prerequisite[i][j] or (is_prerequisite[i][k] and is_prerequisite[k][j])
+        
+        return [is_prerequisite[u][v] for u, v in queries]
