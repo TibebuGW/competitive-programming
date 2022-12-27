c1f720f23d8a654ccbc3a90104ca745a2cb767be
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-12-27 17:54:43 +0300

    Time: 168 ms (83.20%), Space: 69.7 MB (48.72%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        /*
+            i need to know where it doesn't start the violation
+            // if i am starting here
+            // i need to know the position of odds that i have counted
+            this means whenever i am poping a q i know my next index that i am not violating
+        */
+        for (auto &x: nums) {
+            x &= 1;
+        }
+        int last = 0;
+        queue<int> q;
+        int r = 0;
+        int ans = 0;
+        while (r < nums.size()) {
+            if(nums[r]) {
+                q.push(r);
+            }
+            r++;
+            
+            if(q.size() > k) {
+                int n = q.front();
+                last = n+1;
+                q.pop();
+            }
+            
+            if(q.size() == k) {
+                ans += (q.front() - last) + 1;
+            }
+        }
+        return ans;
+    }
+};
