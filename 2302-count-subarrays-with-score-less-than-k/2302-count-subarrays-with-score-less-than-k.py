98f6b4db6f18d639ab7eac544c732b554c9be405
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-12-29 21:35:26 +0300

    Time: 7540 ms (5.43%), Space: 27.6 MB (96.20%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.py
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.py
@@ -1,28 +1,30 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        nums.append(k)
+        prefix = [0]
+        for num in nums: prefix.append(num + prefix[-1])
         ans = 0
-        l = 0
-        total = 0
-        indices = 0
-        last = 0
         
-        for r in range(len(nums)):
-            total += nums[r]
-            indices += 1
+        def findScore(l, r):
+            indices = r - l + 1
+            total = prefix[r + 1] - prefix[l]
+            return total * indices
+        
+        def binarySearch(start):
+            l = start
+            r = len(nums) - 1
+            best = 0
+            while l <= r:
+                mid = (l + r) >> 1
+                score = findScore(start, mid)
+                if score < k:
+                    best = max(best, mid - start + 1)
+                    l = mid + 1
+                else:
+                    r = mid - 1
             
-            if total * indices >= k:
-                max_size = r - l
-                deductable_size = last - l
-                max_subarrays = (max_size * (max_size + 1)) // 2
-                deductable_subarrays = (deductable_size * (deductable_size + 1)) // 2
-                ans += max_subarrays - deductable_subarrays
-                
-                while total * indices >= k:
-                    total -= nums[l]
-                    indices -= 1
-                    l += 1
-                
-                last = r
+            return best
+                    
+        for i in range(len(nums)):
+            ans += binarySearch(i)
         
         return ans
