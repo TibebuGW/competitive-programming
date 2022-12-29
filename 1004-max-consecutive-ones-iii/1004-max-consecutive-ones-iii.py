dabd4a8a4e3330f9194256507d772ebd66bd3bda
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2022-12-29 10:11:46 +0300

    Time: 2528 ms (5.02%), Space: 15.4 MB (8.55%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,17 +1,30 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        total_zeros = 0
-        ans = k
+        ans = 0
+        prefix = [0]
+        for num in nums: prefix.append(num + prefix[-1])
         
-        l = 0
-        for r in range(len(nums)):
-            total_zeros += int(nums[r] == 0)
+        def allOnesInRange(l, r):
+            return prefix[r + 1] - prefix[l]
+        
+        def binarySearch(start):
+            l = start
+            r = len(nums) - 1
+            best = 0
             
-            if total_zeros <= k:
-                ans = max(ans, r - l + 1)
-            else:
-                while total_zeros > k:
-                    total_zeros -= int(nums[l] == 0)
-                    l += 1
+            while l <= r:
+                mid = (l + r) >> 1
+                total_ones = allOnesInRange(start, mid) + k
+                total_size = mid - start + 1
+                if total_ones >= total_size:
+                    best = total_size
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            
+            return best
+        
+        for i in range(len(nums)):
+            ans = max(ans, binarySearch(i))
         
         return ans
