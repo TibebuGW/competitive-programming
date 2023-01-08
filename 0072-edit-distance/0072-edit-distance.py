df1ba1903a48a4be9089d24cac7bc23be5df62fa
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2023-01-08 19:39:02 +0300

    Time: 97 ms (95.84%), Space: 16.8 MB (80.21%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,17 +1,25 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+        
         
         @lru_cache(None)
-        def topdown(p1, p2):
-            if p1 >= len(word1):
-                return len(word2) - p2
-            if p2 >= len(word2):
-                return len(word1) - p1
-            if word1[p1] == word2[p2]:
-                return topdown(p1+1, p2+1)
-
-            insertion = topdown(p1, p2+1)
-            deletion = topdown(p1+1, p2)
-            replace = topdown(p1+1, p2+1)
-            return 1 + min(insertion, deletion, replace)
-        return topdown(0, 0)
+        def dp(idx1 = 0, idx2 = 0):
+            
+            if idx1 == m:
+                return n - idx2
+            
+            if idx2 == n:
+                return m - idx1
+            
+            if word1[idx1] == word2[idx2]:
+                return dp(idx1 + 1, idx2 + 1)
+            
+            insertion = dp(idx1, idx2 + 1)
+            deletion = dp(idx1 + 1, idx2)
+            replacement = dp(idx1 + 1, idx2 + 1)
+            
+            return min(insertion, deletion, replacement) + 1
+        
+        return dp()
