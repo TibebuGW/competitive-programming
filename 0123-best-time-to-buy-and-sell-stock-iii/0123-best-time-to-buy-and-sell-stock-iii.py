82393d1995466a0223d733ef335d34192d8dd5b5
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2023-01-18 20:21:53 +0300

    Time: 4749 ms (12.17%), Space: 671.5 MB (5.00%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        
+        @lru_cache(None)
+        def dp(index = 0, total = 0, canBuy = True):
+            
+            if index == n or total == 2:
+                return 0
+            
+            if canBuy:
+                buy_cur = -prices[index] + dp(index + 1, total, not canBuy)
+                not_buy_cur = dp(index + 1, total, canBuy)
+                return max(buy_cur, not_buy_cur)
+            
+            sell_here = prices[index] + dp(index + 1, total + 1, not canBuy)
+            not_sell_here = dp(index + 1, total, canBuy)
+            return max(sell_here, not_sell_here)
+        
+        return dp()
