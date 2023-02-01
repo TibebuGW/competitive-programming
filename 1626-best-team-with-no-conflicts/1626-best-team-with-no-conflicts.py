b94b0f96495c15a345ba5b05e41a006fe9e6b5df
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2023-02-01 23:18:21 +0300

    Update 1626-best-team-with-no-conflicts.py

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,25 +1,27 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        pair = []
-        for i in range(len(ages)):
-            pair.append((ages[i], scores[i]))
-        pair.sort()
+        n = len(scores)
+        main_array = []
+        for i in range(n):
+            main_array.append([ages[i], scores[i]])
+        main_array.sort()
         
-        memo = [[-1 for _ in range(len(pair))] for _ in range(len(pair))]
-        
-        def dp(memo,prev, pair, i ):
-            if i >= len(pair):
-                return 0
+        memo = {}
+        def dp(cur_index = 0, min_age_index = -1):
             
-            if memo[prev+1][i] != -1:
-                return memo[prev+1][i]
-            jump = dp(memo,prev, pair, i + 1)
-            nojump = 0
-            if prev == -1 or pair[i][1] >= pair[prev][1]:
-                
-                nojump = pair[i][1] + dp(memo, i , pair, i + 1)
-            memo[prev+1][i] = max(jump, nojump)
-                
-            return memo[prev+1][i]
-        
-        return dp(memo, -1, pair, 0)
+            if (cur_index, min_age_index) in memo:
+                return memo[(cur_index, min_age_index)]
+            
+            if cur_index == n:
+                memo[(cur_index, min_age_index)] = 0
+                return memo[(cur_index, min_age_index)]
+            
+            not_take = dp(cur_index + 1, min_age_index)
+            take = 0
+            if min_age_index == -1 or main_array[min_age_index][1] <= main_array[cur_index][1]:
+                take = main_array[cur_index][1] + dp(cur_index + 1, cur_index)
+            
+            
+            memo[(cur_index, min_age_index)] = max(take, not_take)
+            return memo[(cur_index, min_age_index)]        
+        return dp()
