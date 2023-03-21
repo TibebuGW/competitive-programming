863929c96ee227ef4eae524af3d20cc8d242f433
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2023-03-21 21:11:40 +0300

    Time: 6039 ms (14.13%), Space: 14 MB (33.57%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,19 +1,23 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         
-        @lru_cache(None)
-        def options(l = 0, r = len(nums) - 1) -> int:
-        
-            if l > r:
+        # figure out the maximum possible score player 1 can get.
+        def recursion(left, right):
+            
+            # base case
+            if left == right:
+                return nums[right]
+            
+            if left > right:
                 return 0
-            if l == r:
-                return nums[l]
-
-            options1 = nums[l] + min(options(l+1, r-1), options(l+2, r))
-            options2 = nums[r] + min(options(l, r-2), options(l+1, r-1))
-
-            return max(options1, options2)
+            
+            option1 = nums[left] + min(recursion(left + 1, right - 1), recursion(left + 2, right))
+            option2 = nums[right] + min(recursion(left, right - 2), recursion(left + 1, right - 1))
+            
+            return max(option1, option2)
         
+        # check if his score is greater than the max of the total sum.
+        player_one_score = recursion(0, len(nums) - 1)
         total = sum(nums)
-        p1 = options(0, len(nums)-1)
-        return p1 >= total - p1
+        player_two_score = total - player_one_score
+        return player_one_score >= player_two_score
