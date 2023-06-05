80b6d8dd972f2096286e40759ffe7547bea28c79
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2023-06-05 15:15:01 +0300

    Time: 2421 ms (5.01%), Space: 420.6 MB (13.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        max_so_far = prices[-1]
-        max_ = 0
-        
-        for i in range(len(prices)-2, -1, -1):
-            if prices[i] > max_so_far:
-                max_so_far = prices[i]
-            else:
-                max_ = max(max_, max_so_far-prices[i])
-        
-        return max_
+        n = len(prices)
+        @lru_cache(None)
+        def solve(index = 0, status = "to buy"):
+            if index == n:
+                return 0
+            if status == "to buy":
+                buy = solve(index + 1, "to sell") - prices[index]
+                not_buy = solve(index + 1, "to buy")
+                return max(buy, not_buy)
+            else: 
+                sell = prices[index]
+                not_sell = solve(index + 1, status)
+                return max(sell, not_sell)                
+            
+        return max(0, solve())
