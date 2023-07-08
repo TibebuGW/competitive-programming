ab0b81e20839619e02c18fc95c03d526b4a9fb90
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2023-07-08 12:47:23 +0300

    Time: 1595 ms (96.43%), Space: 33.3 MB (13.22%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,17 +1,38 @@
-from sortedcontainers import SortedList
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        sorted_list = SortedList()
-        for i in range(k):
-            sorted_list.add(nums[i])
+        # decreasing monotonic queue
+        # the answer for any subarray of size k is the first element in the queue at that state
+        # for any element you're considering to add to the queue, first remove the element outside of the window from the left of the queue
         
-        arr = [sorted_list[-1]]
+        # initialize a queue
+        queue = deque([])
         
+        # initialize an ans array
+        ans = []
+        
+        # initialize a left pointer
         l = 0
-        for r in range(k, len(nums)):
-            sorted_list.discard(nums[l])
-            sorted_list.add(nums[r])
-            arr.append(sorted_list[-1])
-            l += 1
-        return arr
         
+        # for the first k elements, create an decreasing monotonic
+        for i in range(k):
+            while queue and nums[queue[-1]] < nums[i]:
+                queue.pop()
+            queue.append(i)
+            
+        # append the first element of the queue to the ans array
+        ans.append(nums[queue[0]])
+        
+        # do a for loop from k - len(nums) with variable r
+        # remove nums[l] if it's the first element in the queue
+        # add nums[r] to the queue maintaining monotonicity
+        for r in range(k, len(nums)):
+            if queue[0] == l:
+                queue.popleft()
+            l += 1
+            while queue and nums[queue[-1]] < nums[r]:
+                queue.pop()
+            queue.append(r)
+            ans.append(nums[queue[0]])
+            
+        # return ans
+        return ans
