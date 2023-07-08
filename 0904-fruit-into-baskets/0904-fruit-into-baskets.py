272387c87668d41e4df26f4b951f6c72cafb5f66
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2023-07-08 13:32:01 +0300

    Time: 960 ms (42.28%), Space: 22.2 MB (91.62%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,22 +1,26 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        # initialize your ans variable to 0
         ans = 0
-        l = 0
-        d = {}
         
+        # initialize l = 0
+        l = 0
+        
+        # initialize dct = defaultdict(int)
+        dct = defaultdict(int)
+        
+        # do a for loop to with variable r
+        # inside the for loop create a while loop to decrease the window size until len(dct) > 2
         for r in range(len(fruits)):
-            if fruits[r] in d:
-                d[fruits[r]] += 1
-            else:
-                d[fruits[r]] = 1
-            
-            while len(d) > 2:
-                d[fruits[l]] -= 1
-                if d[fruits[l]] == 0:
-                    del d[fruits[l]]
+            dct[fruits[r]] += 1
+            while l < r and len(dct) > 2:
+                dct[fruits[l]] -= 1
+                if dct[fruits[l]] == 0:
+                    del dct[fruits[l]]
                 l += 1
-                
+            # update ans
             ans = max(ans, r - l + 1)
             
-        
+            
+        # return ans
         return ans
