64d687d4a396dd3bca5e6cb994a84c18f0c14e9f
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2023-07-08 17:10:31 +0300

    Time: 713 ms (59.89%), Space: 30.5 MB (95.68%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,20 +1,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        max_ = float('-inf')
+        sum_so_far = float('-inf')
         
+        for num in nums:
+            if num + sum_so_far < num:
+                sum_so_far = num
+            else:
+                sum_so_far += num
+            max_ = max(max_, sum_so_far)
         
-        pre, suf = [*nums], [*nums]
-        for i in range(1, len(nums)):       pre[i] += max(0, pre[i-1])
-        for i in range(len(nums)-2,-1,-1):  suf[i] += max(0, suf[i+1])
-        # print(pre)
-        # print(suf)
-        def solve(l, r):
-            if l == r:
-                return nums[l]
-            
-            mid = (l+r)//2
-            max_left_subarray = solve(l, mid)
-            max_right_subarray = solve(mid+1, r)
-            # max_crossing_subarray = cross_sum(l, r)
-            return max(max_left_subarray, max_right_subarray, pre[mid]+suf[mid+1])
-        
-        return solve(0, len(nums)-1)
+        return max_
