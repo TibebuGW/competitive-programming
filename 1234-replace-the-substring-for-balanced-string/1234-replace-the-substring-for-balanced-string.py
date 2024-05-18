475b5bc5ad2e648a3decc289bdc94925541f54fb
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-05-18 21:50:30 +0300

    Time: 168 ms (69.26%), Space: 17.3 MB (57.77%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -1,39 +1,35 @@
 class Solution:
     def balancedString(self, s: str) -> int:
-        # find the characters to be deleted
-        # put them in a dictionary along with the count of those characters that need to be deleted
-        # after figuring that out it's a matter of finding the minimum window substring that contains those characters with their respective count
-        target = len(s)//4
         ans = float('inf')
-        
-        char_counts = defaultdict(int)
+        count_dict = defaultdict(int)
         for char in s:
-            char_counts[char] += 1
+            count_dict[char] += 1
         
-        target_dict = defaultdict(int)
-        for key, value in char_counts.items():
-            if value > target:
-                target_dict[key] = value - target
+        needed_count = len(s)//4
+        needed_dict = defaultdict(int)
+        letters = ["Q", "E", "W", "R"]
         
-        if len(target_dict) == 0:
+        for letter in letters:
+            if count_dict[letter] > needed_count:
+                needed_dict[letter] = count_dict[letter] - needed_count
+                
+        if len(needed_dict) == 0:
             return 0
         
-        def is_valid():
-            for char in target_dict:
-                if target_dict[char] > s_dict[char]:
+        current_dict = defaultdict(int)
+        
+        def isValid():
+            for letter, value in needed_dict.items():
+                if needed_dict[letter] > current_dict[letter]:
                     return False
             return True
         
         l = 0
-        s_dict = defaultdict(int)
         for r in range(len(s)):
-            s_dict[s[r]] += 1
-            
-            while is_valid():
+            current_dict[s[r]] += 1
+            while l <= r and isValid():
                 ans = min(ans, r - l + 1)
-                s_dict[s[l]] -= 1
+                current_dict[s[l]] -= 1
                 l += 1
-            
-            
+        
         return ans
-            
