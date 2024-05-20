980cc162f1d7bcbb639e5d6e656a0e54f728efc2
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-05-20 14:35:45 +0300

    Time: 1100 ms (63.39%), Space: 33 MB (36.33%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,38 +1,25 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # decreasing monotonic queue
-        # the answer for any subarray of size k is the first element in the queue at that state
-        # for any element you're considering to add to the queue, first remove the element outside of the window from the left of the queue
-        
-        # initialize a queue
+        ans = []
+        l = 0
         queue = deque([])
         
-        # initialize an ans array
-        ans = []
-        
-        # initialize a left pointer
-        l = 0
-        
-        # for the first k elements, create an decreasing monotonic
         for i in range(k):
-            while queue and nums[queue[-1]] < nums[i]:
+            while queue and queue[-1][0] < nums[i]:
                 queue.pop()
-            queue.append(i)
-            
-        # append the first element of the queue to the ans array
-        ans.append(nums[queue[0]])
+            queue.append((nums[i], i))
         
-        # do a for loop from k - len(nums) with variable r
-        # remove nums[l] if it's the first element in the queue
-        # add nums[r] to the queue maintaining monotonicity
-        for r in range(k, len(nums)):
-            if queue[0] == l:
+        ans.append(queue[0][0])
+        
+        for i in range(k, len(nums)):
+            if queue and queue[0][1] == l:
                 queue.popleft()
             l += 1
-            while queue and nums[queue[-1]] < nums[r]:
-                queue.pop()
-            queue.append(r)
-            ans.append(nums[queue[0]])
             
-        # return ans
+            while queue and queue[-1][0] < nums[i]:
+                queue.pop()
+            queue.append((nums[i], i))
+            
+            ans.append(queue[0][0])
+        
         return ans
