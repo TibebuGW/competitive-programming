fd26a67f65144d62aa7a546de251272769758b69
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-05-20 13:56:12 +0300

    Time: 1600 ms (5.03%), Space: 31.1 MB (20.48%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -1,40 +1,38 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        # reverse sort the array
-        # binary search on the answer
-        # for each size on the binary search, check if it's possible to use the operations and return true/false
-        # find the best size
+        nums.sort()
+        n = len(nums)
+        ans = 1
         
-        nums.sort(reverse = True)
-        
-        def possibleWindow(size):
-            
+        def checkWindow(size):
             total = 0
+            l = 0
             for i in range(size):
                 total += nums[i]
             
-            l = 0
-            if total + k >= nums[l] * size:
+            r = size
+            if size * (nums[r - 1]) <= total + k:
                 return True
             
-            for r in range(size, len(nums)):
-                total += nums[r]
+            for r in range(size, n):
                 total -= nums[l]
-                l += 1
-                if total + k >= nums[l] * size:
+                total += nums[r]
+                if size * (nums[r]) <= total + k:
                     return True
+                l += 1
             
             return False
         
-        l = 1
-        r = len(nums)
-        best = 1
-        while l <= r:
-            mid = (l + r)//2
-            if possibleWindow(mid):
-                best = mid
-                l = mid + 1
-            else:
-                r = mid - 1
+        left = 1
+        right = n
         
-        return best
+        while left <= right:
+            mid = (left + right) // 2
+            possible = checkWindow(mid)
+            if possible:
+                ans = max(ans, mid)
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return ans
