b5638396a3d52d08b5057e23c6d81afcd25c8263
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-05-20 16:05:45 +0300

    Time: 1119 ms (25.00%), Space: 27.7 MB (15.69%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.py
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.py
@@ -1,48 +1,37 @@
-from collections import deque
 class Solution:
     def maximumRobots(self, chargeTimes: List[int], runningCosts: List[int], budget: int) -> int:
         n = len(chargeTimes)
-        def isPossible(size):
-            total_runningCosts = 0
-            queue_window = deque()
-            
-            for i in range(size):
-                total_runningCosts += runningCosts[i]
-                while queue_window and chargeTimes[queue_window[-1]] < chargeTimes[i]:
-                    queue_window.pop()
-                queue_window.append(i)
-            
-            max_chargeTimes = chargeTimes[queue_window[0]]
-            if max_chargeTimes+(size*total_runningCosts) <= budget:
-                return True
-            
-            l = 0
-            for r in range(size, n):
-                total_runningCosts -= runningCosts[l]
-                if chargeTimes[queue_window[0]] == chargeTimes[l]:
-                    queue_window.popleft()
-                l += 1
-                total_runningCosts += runningCosts[r]
-                while queue_window and chargeTimes[queue_window[-1]] < chargeTimes[r]:
-                    queue_window.pop()
-                queue_window.append(r)
-                max_chargeTimes = chargeTimes[queue_window[0]]
-                if max_chargeTimes+(size*total_runningCosts) <= budget:
-                    return True
-            
-            return False
-        
-        
-        left = 1
-        right = n
         ans = 0
-        while left <= right:
-            mid = (left+right)//2
-            possible = isPossible(mid)
-            if possible:
-                ans = mid
-                left = mid+1
-            else:
-                right = mid-1
+        queue = deque([])
+        total = 0
+        def cost_calculator(k):
+            if not len(queue):
+                return 0
+            return queue[0][0] + (k * total)
+        
+        l = 0
+        r = 0
+        while r < n:
+            total += runningCosts[r]
+            while queue and queue[-1][0] < chargeTimes[r]:
+                queue.pop()
+            queue.append((chargeTimes[r], r))
+            
+            k = r - l + 1
+                
+            while cost_calculator(k) > budget:
+                if queue and queue[0][1] == l:
+                    queue.popleft()
+                
+                total -= runningCosts[l]
+                l += 1
+                k = r - l + 1
+            
+            cost = cost_calculator(k)
+            if cost <= budget:
+                ans = max(ans, k)
+            
+            r += 1
+        
         
         return ans
