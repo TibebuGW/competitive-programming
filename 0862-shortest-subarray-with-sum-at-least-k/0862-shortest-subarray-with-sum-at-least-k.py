e3087c4d8d375805222bd5a8d7b9538a5bacdb47
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-05-23 11:33:59 +0000

    Time: 899 ms (51.53%), Space: 31.1 MB (45.60%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,14 +1,26 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        prefix = [0] + list(accumulate(nums))
-        ans = float('inf')
-        queue = deque()
+        n = len(nums)
+        ans = n + 1
+        total = nums[0]
+        if total >= k:
+            return 1
         
-        for i in range(len(prefix)):
-            while queue and prefix[i] <= prefix[queue[-1]]:
-                queue.pop()
-            queue.append(i)
+        queue = deque([(nums[0], 0)])
+        
+        for i in range(1, len(nums)):
+            total += nums[i]
             
-            while queue and prefix[queue[-1]] - prefix[queue[0]] >= k:
-                ans = min(ans, queue[-1] - queue.popleft())
-        return ans if ans != float('inf') else -1
+            if total >= k:
+                ans = min(ans, i + 1)
+            
+            
+            while queue and total - queue[0][0] >= k:
+                ans = min(ans, i - queue[0][1])
+                queue.popleft()
+            
+            while queue and queue[-1][0] >= total:
+                queue.pop()
+            queue.append((total, i))
+            
+        return -1 if ans == n + 1 else ans
