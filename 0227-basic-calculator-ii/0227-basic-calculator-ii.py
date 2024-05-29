5ea30ccdd9d87e456e41bd2a9f3bd62e03a89a54
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-05-29 17:26:06 +0000

    Time: 178 ms (9.86%), Space: 19.6 MB (67.01%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,44 +1,45 @@
 class Solution:
-    def applyOperation(self, num1, num2, op):
-        if op == "+": return num1 + num2
-        if op == "-": return num1 - num2
-        if op == "/": return num1 // num2
-        if op == "*": return num1 * num2
-        
     def calculate(self, s: str) -> int:
-        precedence = {"+": 1, "-": 1, "*": 2, "/": 2}
-        values = []
-        ops = []
+        operator_stack = deque([])
+        digit_stack = deque([])
+        if s[0] == "-":
+            digit_stack.append(0)
         
         i = 0
-        
         while i < len(s):
-            char = s[i]
-            if char == " ":
+            if s[i] == " ":
                 i += 1
                 continue
-            elif char.isdigit():
-                i += 1
-                val = int(char)
+            elif s[i].isdigit():
+                temp = []
                 while i < len(s) and s[i].isdigit():
-                    val = (val*10) + int(s[i])
+                    temp.append(s[i])
                     i += 1
+                
                 i -= 1
-                values.append(val)
+                digit_stack.append(int("".join(temp)))
+                if operator_stack and (operator_stack[-1] == "*" or operator_stack[-1] == "/"):
+                    num1 = digit_stack.pop()
+                    num2 = digit_stack.pop()
+                    if operator_stack[-1] == "*":
+                        digit_stack.append(num1 * num2)
+                    else:
+                        digit_stack.append(num2 // num1)
+                    operator_stack.pop()
             else:
-                while ops and precedence[ops[-1]] >= precedence[char]:
-                    val2 = values.pop()
-                    val1 = values.pop()
-                    op = ops.pop()
-                    values.append(self.applyOperation(val1, val2, op))
-                ops.append(char)
+                operator_stack.append(s[i])
             
             i += 1
         
-        while ops:
-            val2 = values.pop()
-            val1 = values.pop()
-            op = ops.pop()
-            values.append(self.applyOperation(val1, val2, op))
+        while operator_stack:
+            num1 = digit_stack.popleft()
+            num2 = digit_stack.popleft()
+            if operator_stack[0] == "+":
+                digit_stack.appendleft(num1 + num2)
+            else:
+                digit_stack.appendleft(num1 - num2)
+            operator_stack.popleft()
         
-        return values[0]
+        assert(len(digit_stack) == 1)
+        
+        return digit_stack[0]
