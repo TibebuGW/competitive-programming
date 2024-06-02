65a390816f8b196040a389736235e492b660b787
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-06-02 19:35:41 +0000

    Time: 54 ms (9.79%), Space: 18 MB (8.12%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -12,11 +12,8 @@ class Solution:
                 else:
                     queue.append((index, char))
         
-        if not queue or queue[0][0] != 0:
-            queue.appendleft((-1, ""))
-        
-        if not queue or queue[-1][0] != len(s) - 1:
-            queue.append((len(s), ""))
+        queue.appendleft((-1, ""))
+        queue.append((len(s), ""))
         
         for i in range(1, len(queue)):
             ans = max(ans, queue[i][0] - queue[i - 1][0] - 1)
