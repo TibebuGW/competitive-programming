941d24b95543edf5fa753302ea9e5610bc80cf0d
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-06-05 12:48:57 +0000

    Time: 4762 ms (9.19%), Space: 18.1 MB (8.53%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        dp = [float('inf') for _ in range(len(nums))]
-        dp[-1] = 0
-        n = len(nums)
+        possible_indices = deque([[len(nums) - 1, 0]])
         
-        for i in range(n - 2, -1, -1):
-            min_length = float('inf')
-            for j in range(i + 1, min(n, nums[i] + i + 1)):
-                min_length = min(min_length, dp[j])
-            
-            dp[i] = min_length + 1
+        for i in range(len(nums) - 2, -1, -1):
+            j = 0
+            max_reach = i + nums[i]
+            cur_min = float('inf')
+            while j < len(possible_indices) and max_reach >= possible_indices[j][0]:
+                cur_min = min(cur_min, possible_indices[j][1])
+                j += 1
+            if cur_min < float('inf'):
+                possible_indices.appendleft([i, cur_min + 1])
         
-        return dp[0]
+        return possible_indices[0][1]
