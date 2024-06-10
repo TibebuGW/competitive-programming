f479b8319a606977b351f8773f1bed8de8a404a8
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-06-10 11:34:01 +0000

    Time: 50 ms (73.43%), Space: 17 MB (98.52%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,37 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        l = 0
-        r = len(nums) - 1
-        min_ = float('inf')
-        min_index = 0
-        for i in range(1, len(nums)):
-            if nums[i] < nums[i - 1]:
-                min_index = i
-                break
-                
-        if min_index == 0:
-            if  not (nums[0] <= target <= nums[-1]):
-                return False
-            l = 0
-            r = len(nums) - 1
-        else:
-            if not (nums[min_index] <= target <= nums[min_index - 1]):
-                return False
-            
-            if nums[min_index] <= target <= nums[-1]:
-                l = min_index
-                r = len(nums) - 1
-            else:
-                l = 0
-                r = min_index - 1
+        left = 0
+        right = len(nums)-1
         
-        while l <= r:
-            mid = (l + r) // 2
+        while left <= right:
+            mid = (left+right)//2
+            
             if nums[mid] == target:
                 return True
-            elif nums[mid] < target:
-                l = mid + 1
+            
+            if nums[left] == nums[mid] == nums[right]:
+                left += 1
+                right -= 1
+            elif nums[left] > nums[mid]:
+                if target >= nums[mid] and target <= nums[right]:
+                    left = mid+1
+                else:
+                    right = mid-1
             else:
-                r = mid - 1
-        
+                if target >= nums[left] and target <= nums[mid]:
+                    right = mid-1
+                else:
+                    left = mid+1
+            
         return False
