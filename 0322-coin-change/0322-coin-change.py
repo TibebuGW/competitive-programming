b73ec25d2b0ade84c38b599fbfa1f6e0cacc679f
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-06-29 23:26:01 +0000

    Time: 329 ms (98.65%), Space: 17.4 MB (34.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,26 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n = len(coins)
+        if amount == 0:
+            return 0
+        queue = deque([amount])
+        level = 1
+        s = set()
         
-        @lru_cache(None)
-        def dp(idx = n - 1, total = amount):
-            
-            if total == 0:
-                return 0
-            
-            if idx == 0:
-                remainder = total
-                if remainder % coins[idx] == 0:
-                    return remainder // coins[idx]
-                return float('inf')
-            
-            not_take = dp(idx - 1, total)
-            take = float('inf')
-            
-            if total >= coins[idx]:
-                take = 1 + dp(idx, total - coins[idx])
-            
-            return min(take, not_take)
-        
-        ans = dp()
-        return ans if ans != float('inf') else -1
+        while queue:
+            n = len(queue)
+            for i in range(n):
+                val = queue.popleft()
+                for coin in coins:
+                    cur = val - coin
+                    if cur == 0:
+                        return level
+                    if cur > 0 and cur not in s:
+                        s.add(cur)
+                        queue.append(cur)
+            level += 1
+        return -1
