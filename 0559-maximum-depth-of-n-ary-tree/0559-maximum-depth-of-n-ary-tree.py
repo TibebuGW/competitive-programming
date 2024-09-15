4ee97cce34ec416bd737f0ddda1fcce1976d3e88
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-09-15 14:46:14 +0300

    Time: 46 ms (38.26%), Space: 18.1 MB (75.54%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        
+        max_ = 0
+        for child in root.children:
+            max_ = max(max_, self.maxDepth(child))
+        
+        return max_ + 1
