4e8e92541d5964139e1b4b4718a6ee536f97a3f1
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-09-17 18:37:58 +0300

    Time: 889 ms (20.58%), Space: 20.7 MB (7.77%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -2,22 +2,25 @@ class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         n = len(heights)
         m = len(heights[0])
-        efforts = defaultdict(lambda: float('inf'))
-        efforts[(0,0)] = 0
-        queue = [(0,(0,0))] # effort_so_far, cell
-        directions = [[-1, 0], [0, -1], [0, 1], [1, 0]]
-        in_range = lambda row, col: 0 <= row < n and 0 <= col < m
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        in_range = lambda x, y: 0 <= x < n and 0 <= y < m
+        distances = {}
         
+        queue = [(0, 0, 0)]
+        visited = set()
         
         while queue:
-            effort_so_far, cell = heappop(queue)
+            cost, x, y = heapq.heappop(queue)
+            if (x, y) in visited:
+                continue
+            
+            visited.add((x, y))
+            distances[(x, y)] = cost
             
             for row_move, col_move in directions:
-                new_row = cell[0] + row_move
-                new_col = cell[1] + col_move
+                new_row = row_move + x
+                new_col = col_move + y
                 if in_range(new_row, new_col):
-                    if max(effort_so_far, abs(heights[new_row][new_col] - heights[cell[0]][cell[1]])) < efforts[(new_row, new_col)]:
-                        efforts[(new_row, new_col)] = max(effort_so_far, abs(heights[new_row][new_col] - heights[cell[0]][cell[1]]))
-                        heappush(queue, (efforts[(new_row, new_col)], (new_row, new_col)))
+                    heapq.heappush(queue, (max(cost, abs(heights[x][y] - heights[new_row][new_col])), new_row, new_col))
         
-        return efforts[(n - 1, m - 1)]
+        return distances[(n - 1, m - 1)]
