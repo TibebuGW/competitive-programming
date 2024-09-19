8a01c946cfacea754cbd305054d7aa4039b91d28
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-09-19 22:01:15 +0300

    Time: 27 ms (97.87%), Space: 16.6 MB (83.24%) - LeetHub

--- 0843-guess-the-word/0843-guess-the-word.py
+++ 0843-guess-the-word/0843-guess-the-word.py
@@ -1,3 +1,4 @@
+import random
 # """
 # This is Master's API interface.
 # You should not implement it, or speculate about its implementation
@@ -7,29 +8,23 @@
 
 class Solution:
     def findSecretWord(self, words: List[str], master: 'Master') -> None:
-        ruled_out = set()
-        
-        def pickRandom(candidates):
-            return random.randint(0, len(candidates) - 1)
         
         def matcher(word1, word2):
             count = 0
-            for i in range(6):
+            for i in range(len(word1)):
                 if word1[i] == word2[i]:
                     count += 1
             return count
         
-        
         while True:
-            index = pickRandom(words)
-            guess = master.guess(words[index])
+            cur_word = random.choice(words)
+            guess = master.guess(cur_word)
             if guess == 6:
                 return
+
+            candidates = []
+            for word in words:
+                if matcher(cur_word, word) == guess:
+                    candidates.append(word)
             
-            word = words[index]
-            new_words_list = []
-            for word1 in words:
-                if matcher(word, word1) == guess:
-                    new_words_list.append(word1)
-            
-            words = new_words_list
+            words = candidates
