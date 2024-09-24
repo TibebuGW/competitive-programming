25ba64dd381c37d3e1f98d71b79a086db8f01075
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-09-24 16:07:45 +0300

    Time: 1294 ms (58.83%), Space: 46.2 MB (43.02%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.py
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.py
@@ -1,29 +1,25 @@
 class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
-        incoming = [[0,0] for i in range(n)]
-        totalTime = 0
+        node_info = [[0, 0] for _ in range(n)] # [in_degree, max_time_spent_before_this_course]
+        ans = 0
         graph = defaultdict(list)
+        for u, v in relations:
+            graph[u - 1].append(v - 1)
+            node_info[v - 1][0] += 1
         
-        for prev, cour in relations:
-            graph[prev - 1].append(cour - 1)
-            incoming[cour - 1][0] += 1
-            
-        que = deque()
+        queue = deque([])
+        
         for i in range(n):
-            if incoming[i][0] == 0:
-                que.append((i, time[i]))
-                
-        while que:
-            size = len(que)
-            for _ in range(size):
-                curr, maxTime = que.popleft()
-                totalTime = max(totalTime, maxTime)
-                
-                for courses in graph[curr]:
-                    incoming[courses][0] -= 1
-                    incoming[courses][1] = max(incoming[courses][1], maxTime)
-                    if incoming[courses][0] == 0:
-                        
-                        que.append((courses, incoming[courses][1] + time[courses]))
-       
-        return totalTime
+            if node_info[i][0] == 0:
+                queue.append((i, time[i])) # it takes time[i] months to finish course i and all it's prerequisites
+        
+        while queue:
+            node, max_time_spent_before_this_course = queue.popleft()
+            ans = max(ans, max_time_spent_before_this_course)
+            for nei in graph[node]:
+                node_info[nei][0] -= 1
+                node_info[nei][1] = max(node_info[nei][1], max_time_spent_before_this_course)
+                if node_info[nei][0] == 0: # all prerequisites are finished
+                    queue.append((nei, node_info[nei][1] + time[nei]))
+        
+        return ans
