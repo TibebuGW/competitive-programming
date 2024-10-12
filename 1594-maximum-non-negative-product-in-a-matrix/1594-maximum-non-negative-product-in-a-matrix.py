5827a6f98a437fa198916d3e8a8611cb3282c6c0
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-10-12 18:11:32 +0300

    Time: 60 ms (15.77%), Space: 17.3 MB (7.88%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
@@ -1,35 +1,33 @@
 class Solution:
     def maxProductPath(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        in_range = lambda row, col: 0 <= row < m and 0 <= col < n
-        MOD = (10**9) + 7
+        n = len(grid)
+        m = len(grid[0])
+        in_range = lambda x, y: 0 <= x < n and 0 <= y < m
         
         @lru_cache(None)
         def dp(i = 0, j = 0):
             if not in_range(i, j):
                 return []
             
-            if i == m - 1 and j == n - 1:
+            if i == n - 1 and j == m - 1:
                 return [grid[i][j], grid[i][j]]
             
             right = dp(i, j + 1)
             down = dp(i + 1, j)
             
-            cur_max = -(4**15)
-            cur_min = 4**15
+            max_ = -(10**6)
+            min_ = 10**6
             for val in right:
-                cur_max = max(cur_max, val*grid[i][j])
-                cur_min = min(cur_min, val*grid[i][j])
+                max_ = max(max_, val*grid[i][j])
+                min_ = min(min_, val*grid[i][j])
             
             for val in down:
-                cur_max = max(cur_max, val*grid[i][j])
-                cur_min = min(cur_min, val*grid[i][j])
+                max_ = max(max_, val*grid[i][j])
+                min_ = min(min_, val*grid[i][j])
                 
-            return [cur_max, cur_min]
-        
-        max_ans, min_ans =  dp()
-        if max_ans < 0:
+            return [min_, max_]
+            
+        ans = dp()[1]
+        if ans < 0:
             return -1
-        else:
-            return max_ans%MOD
+        return ans % 1_000_000_007 
