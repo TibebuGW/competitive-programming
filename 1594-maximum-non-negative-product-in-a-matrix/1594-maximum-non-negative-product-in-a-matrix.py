f65d3a4045159347f59cb8a08351a7ab2da16e11
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-10-12 18:36:27 +0300

    Time: 218 ms (6.64%), Space: 61.3 MB (5.39%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
@@ -3,31 +3,21 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         in_range = lambda x, y: 0 <= x < n and 0 <= y < m
+        ans = [-1]
+        
         
         @lru_cache(None)
-        def dp(i = 0, j = 0):
+        def dp(i = 0, j = 0, total = 1):
             if not in_range(i, j):
-                return []
+                return
             
             if i == n - 1 and j == m - 1:
-                return [grid[i][j], grid[i][j]]
+                ans[0] = max(ans[0], total * grid[i][j])
+                return
             
-            right = dp(i, j + 1)
-            down = dp(i + 1, j)
+            right = dp(i, j + 1, total * grid[i][j])
+            down = dp(i + 1, j, total * grid[i][j])
             
-            max_ = -(10**6)
-            min_ = 10**6
-            for val in right:
-                max_ = max(max_, val*grid[i][j])
-                min_ = min(min_, val*grid[i][j])
-            
-            for val in down:
-                max_ = max(max_, val*grid[i][j])
-                min_ = min(min_, val*grid[i][j])
-                
-            return [min_, max_]
-            
-        ans = dp()[1]
-        if ans < 0:
-            return -1
-        return ans % 1_000_000_007 
+        
+        dp()
+        return ans[0] % 1_000_000_007 if ans[0] != -1 else -1
