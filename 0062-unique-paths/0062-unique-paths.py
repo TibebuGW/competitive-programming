f46d6b35816bd6edd6f3dc71cfc63987d13665d9
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-10-14 16:48:50 +0300

    Time: 40 ms (22.92%), Space: 17 MB (6.15%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,15 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
-        in_bound = lambda r, c: 0 <= r < m and 0 <= c < n
+        in_range = lambda x, y: 0 <= x < m and 0 <= y < n
         
         @lru_cache(None)
         def dp(i = 0, j = 0):
-            if not in_bound(i, j):
-                return 0
-            
             if i == m - 1 and j == n - 1:
                 return 1
+            
+            if not in_range(i, j):
+                return 0
             
             right = dp(i, j + 1)
             down = dp(i + 1, j)
