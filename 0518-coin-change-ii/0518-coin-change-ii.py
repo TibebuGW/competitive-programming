565240fd44831859a37c38d114f61e3fb75ec029
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-10-14 15:59:51 +0300

    Time: 762 ms (14.32%), Space: 278.6 MB (5.42%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,24 +1,19 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        n = len(coins)
+        
         
         @lru_cache(None)
-        def dp(idx = n - 1, total = amount):
-            
+        def dp(idx = 0, total = amount):
+            if idx == len(coins):
+                return 0
             if total == 0:
                 return 1
             
-            if idx == 0:
-                if total % coins[idx] == 0:
-                    return 1
-                return 0
-            
-            
-            not_take = dp(idx - 1, total)
+            not_take = dp(idx + 1, total)
             take = 0
             if total >= coins[idx]:
                 take = dp(idx, total - coins[idx])
             
-            return not_take + take
+            return take + not_take
         
         return dp()
