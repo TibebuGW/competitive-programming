ff5c0477b5dc0cf0aff3d02e0e3ca83ecefc3a19
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-10-15 14:00:06 +0300

    Time: 40 ms (51.80%), Space: 16.8 MB (18.36%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,20 +1,20 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        ans = [[]]
+        ans = []
         nums.sort()
-        def backtrack(path = [], index = 0):
-            if path:
-                ans.append(path[:])
-            
-            visited_at_level = set()
-            for i in range(index, len(nums)):
-                if nums[i] not in visited_at_level:
-                    path.append(nums[i])
-                    backtrack(path, i+1)
-                    path.pop()
-
-                    visited_at_level.add(nums[i])
-                
         
+        def backtrack(i = 0, path = []):
+            if i == len(nums):
+                ans.append(path[::])
+                return
+            
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+            idx = i
+            while i < len(nums) and nums[i] == nums[idx]:
+                i += 1
+            backtrack(i, path)
+            
         backtrack()
         return ans
