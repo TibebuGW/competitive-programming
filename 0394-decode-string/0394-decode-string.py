2bd248831f1b44eb2f825ab98501210925559da0
TibebuGW <73777766+TibebuGW@users.noreply.github.com>
2024-10-16 18:59:21 +0300

    Time: 38 ms (28.71%), Space: 16.6 MB (42.08%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,28 +1,36 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
+        ans = []
         i = 0
+        
+        def rec():
+            nonlocal i
+            
+            digits = []
+            while s[i].isdigit():
+                digits.append(s[i])
+                i += 1
+            
+            num = int("".join(digits))
+            
+            i += 1
+            chars = []
+            while s[i] != "]":
+                if s[i].isalpha():
+                    chars.append(s[i])
+                elif s[i].isdigit():
+                    chars.append(rec())
+                i += 1
+            
+            return num * "".join(chars)
         
         while i < len(s):
             char = s[i]
-            if char != "[" and char != "]":
-                if char.isdigit():
-                    digit_array = []
-                    while s[i].isdigit():
-                        digit_array.append(s[i])
-                        i += 1
-                    i -= 1
-                    stack.append("".join(digit_array))
-                else:
-                    stack.append(char)
-            elif char == "]":
-                cur_string = []
-                while not stack[-1].isdigit():
-                    cur_string.append(stack.pop())
-                
-                multiplier = stack.pop()
-                stack.append(int(multiplier) * "".join(cur_string[::-1]))
-            
+            if char.isalpha():
+                ans.append(char)
+            elif char.isdigit():
+                ans.append(rec())
             i += 1
         
-        return "".join(stack)
+        return "".join(ans)
+                
